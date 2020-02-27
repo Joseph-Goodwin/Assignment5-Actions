20d58f13af87ec6289fa4a2077999fcb7080242d
Joseph Goodwin <jgoodwin9628@gmail.com>
2020-02-27 11:06:54 -0800

    Added first function

--- task.py
+++ task.py
@@ -1,2 +1,10 @@
+import math
+
+
 def firstrun():
     return "success"
+
+def pi():
+    radius = 3
+    area = float(radius**2) * float(math.pi)
+    return int(area)
--- tests.py
+++ tests.py
@@ -12,6 +12,10 @@ class TestCase(unittest.TestCase):
         expected = "failure"
         self.assertNotEqual(expected, task.firstrun())
 
+    def test_pi(self):
+        expected = 28
+        self.assertEqual(expected, task.pi())
+
 
 if __name__ == '__main__':
     unittest.main()
